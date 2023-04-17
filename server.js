8fda422ca4d913a4d735d5a386e79f7d95c54bf6
Heisenburs <jada,burs@gmail.com>
2023-04-17 10:18:06 -0400

    server set up & listening

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const { log } = require('console');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//TODO: Data
+// const pokemon = require('./models/pokemon')
+
+//* Configuration
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine())
+
+//* Middleware
+app.use((req, res, next) => {
+    console.log(req.body);
+    console.log('I run for all the routes')
+    next();
+    //? next() - ensures that the request continues to be processed and that you get a response
+})
+
+app.use(express.urlencoded({extended: false}))
+
+//* Homepage
+app.get('/',(req,res) => {
+    res.send(`Welcome to the Pokemon App!`)
+})
+
+
+
+
+//* Listening
+app.listen(8000, () => {
+    console.log(`Port 8000: Listening...`);
+    //? connecting to mongoDB, can be done in other folder
+    mongoose.set('strictQuery', true)
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    mongoose.connection.once('open', () => {
+        console.log('Connected to MongoDB!');
+    })
+})
